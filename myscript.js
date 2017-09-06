8926b093ff8ef859c32e6f2e97edca12c1682d88
camsqu <camsqu@gmail.com>
2017-09-06 11:05:25 -0700

    maybe this does a thing

--- myscript.js
+++ myscript.js
@@ -1,5 +1,9 @@
 $(document).ready({
   $("#q1").click(function() {
-    $("#govquiz").show
+    $("#govquiz1").show
+  })
+  $("#q1a").click(function () {
+    $("#govquiz1").hide
+    $("#govans1").show
   })
 });
