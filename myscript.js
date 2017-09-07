09546022bcbdfb769ff96e6dce8b064f56cf5519
camsqu <camsqu@gmail.com>
2017-09-07 00:36:35 -0700

    so much easier now that it's linked

--- myscript.js
+++ myscript.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
   $("#q1").click(function() {
-    $("#govquiz1").show();
+    $("#govquiz").show();
   });
   $("#q1a").click(function () {
     $("#gov1a").show();
+    $("#q1a").hide();
+    $("#next1").show();
+  });
+  $("#next1").click(function() {
+    $("#next1").hide();
+    $("#gov1").hide();
+    $("#gov1a").hide();
+    $("#gov2").show();
+  });
+  $("#q2a").click(function () {
+    $("#gov2a").show();
+    $("#q2a").hide();
+    $("#next2").show();
   });
 });
