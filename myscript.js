0f742dfcca97d91bed89133938cb8ad9a16532ed
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2018-01-29 09:37:35 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,40 +1,4 @@
 $(document).ready(function(){
-  $("#q1").click(function() {
-    $("#govquiz").show();
-    $("#gov3").hide();
-    $("#gov3a").hide();
-    $("#gov1").show();
-    $("#q1a").show();
-  });
-  $("#q1a").click(function () {
-    $("#gov1a").show();
-    $("#q1a").hide();
-    $("#next1").show();
-  });
-  $("#next1").click(function() {
-    $("#next1").hide();
-    $("#gov1").hide();
-    $("#gov1a").hide();
-    $("#gov2").show();
-    $("#q2a").show();
-  });
-  $("#q2a").click(function () {
-    $("#gov2a").show();
-    $("#q2a").hide();
-    $("#next2").show();
-  });
-  $("#next2").click(function() {
-    $("#next2").hide();
-    $("#gov2").hide();
-    $("#gov2a").hide();
-    $("#gov3").show();
-    $("#q3a").show();
-  });
-  $("#q3a").click(function () {
-    $("#gov3a").show();
-    $("#q3a").hide();
-    $("#finish").show();
-  });
   $("#finish").click(function () {
     $("#govquiz").hide();
     $("#finish").hide();
