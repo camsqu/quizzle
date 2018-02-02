c41e8e578487040cbb0106f46857e06c35b109da
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2018-02-02 09:47:40 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -49,15 +49,15 @@ def checkScore():
 
 def get_score(answers):
     score = 100
-      if answers[0] == "45":
-        print("this is a check")
-        score += 100
-      if answers[2] == "1776":
-        print("this is a check")
-        score += 100
-      if answers[1] == "Federal Government":
-        print("this is a check")
-        score += 100
-      return "Score: " + str(score)
+    if answers[0] == "45":
+      print("this is a check")
+      score += 100
+    if answers[2] == "1776":
+      print("this is a check")
+      score += 100
+    if answers[1] == "Federal Government":
+      print("this is a check")
+      score += 100
+    return "Score: " + str(score)
 if __name__=="__main__":
     app.run(debug=True)
